be93eeca49bc90a437df436b20d939f2e8365ef2
Cyrisub <git@cyrisbot.com>
2025-03-29 22:48:07 +0800

    Init

---
+++ Source/UnrealToml/Private/UnrealToml.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealToml.h"
+
+#define LOCTEXT_NAMESPACE "FUnrealTomlModule"
+
+void FUnrealTomlModule::StartupModule()
+{
+}
+
+void FUnrealTomlModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FUnrealTomlModule, UnrealToml)
